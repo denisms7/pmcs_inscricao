ffdb1a34232299abb82d068026501ffdba03fdc6
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2023-08-08 10:23:01 -0300

    added

--- app/inscricao/models.py
+++ app/inscricao/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Incricao(models.Model):
+    MODALIDADE_CHOICES = [
+        ('1', _('Solo')),
+        ('2', _('Dupla')),
+        ('3', _('Grupo')),
+        ('4', _('Outros')),
+    ]
+
+    data = models.DateTimeField(auto_now_add=True, verbose_name=_('Data'))
+    modalidade = models.PositiveSmallIntegerField(choices=MODALIDADE_CHOICES, verbose_name=_('Modalidade'), null=True, blank=True)
+
+    def __str__(self):
+        return f" - {self.modalidade}"
